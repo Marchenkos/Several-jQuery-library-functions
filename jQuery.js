c65fbd3e9abf2922de2d5c2005daedd779a6b88a
Kseniya Marchanka <Kseniya.Marchanka@itechart-group.com>
2019-09-19 18:43:15 +0300

    Create a frame

---
+++ jQuery.js
@@ -0,0 +1,54 @@
+let myjQuery = function(x) {
+    if(x.includes("#")) {
+
+    }
+
+    if(x.includes(".")) {
+        
+    }
+
+    if(x.includes("#")) {
+        
+    }
+    return {
+        addClass: function() {
+            return this;
+        },
+        removeClass: function() {
+            return this;
+        },
+        append: function() {
+            return this;
+        },
+        remote: function() {
+            return this;
+        },
+        text: function() {
+            return this;
+        },
+        attr: function() {
+            return this;
+        },
+        children: function() {
+            return this;
+        },
+        empty: function() {
+            return this;
+        },
+        css: function() {
+            return this;
+        },
+        click: function() {
+            return this;
+        },
+        each: function() {
+            return this;
+        },
+        toggle: function() {
+            return this;
+        },
+        wrap: function() {
+            return this;
+        }
+    }
+}
