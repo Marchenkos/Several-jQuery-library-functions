e041328524d37fd2119a34036ebcf2991be90b54
Ksenia Marchenko <ms.ksi2000@mail.ru>
2019-09-22 13:47:18 +0300

    Add eslint. Change code style. Change remove method

--- jQuery.js
+++ jQuery.js
@@ -1,123 +1,151 @@
-let myjQuery = function(value) {
-    let elementList = document.querySelectorAll(value);
+document.addEventListener("DOMContentLoaded", () => {
+    const myjQuery = function (value) {
+        const elementList = document.querySelectorAll(value);
 
-    return {
-        addClass: function(newClass) {
-            for (let i = 0; i < elementList.length; ++i) {
-                elementList[i].classList.add(newClass);
-            }
-
-            return this;
-        },
-        removeClass: function(remoteClass) {
-            for (let i = 0; i < elementList.length; ++i) {
-                elementList[i].classList.remove(remoteClass);
-            }
-
-            return this;
-        },
-        append: function(data) {
-            let lastValue = elementList[0].innerHTML;
-            elementList[0].innerHTML = lastValue + " " +data;
-
-            return this;
-        },
-        remove: function(elementName) {
-            let element = document.querySelector(elementName);
-            element.remove();
-
-            return this;
-        },
-        text: function(value) {
-            if (value == null) {
-                let commonValue = "";
-
+        return {
+            addClass(newClass) {
                 for (let i = 0; i < elementList.length; ++i) {
-                    commonValue = commonValue + elementList[i].innerHTML + " ";
+                    elementList[i].classList.add(newClass);
                 }
 
-                return commonValue;
-             } else {
-                let item = elementList[0];
-                item.innerHTML = data;
-            }
-
-            return this;
-        },
-        attr: function(attribute, value) {
-            if (value == null) {
-                let value = elementList[0].getAttribute(attribute);
-            } else {
-                elementList[0].setAttribute(attribute, value);
-            }
-       
-            return this;
-        },
-        children: function() {
-            let childNode = [];
-
-            for(elem of elementList) {
-                childNode.push(elem.childNodes);
-            }
-
-            return;
-        },
-        empty: function() {
-            for (let i = 0; i < elementList.length; ++i) {
-                elementList[i].innerHTML = "";
-            }
-
-            return this;
-        },
-        css: function(prop, value) {
-            if (value != null) {
+                return this;
+            },
+            removeClass(remoteClass) {
                 for (let i = 0; i < elementList.length; ++i) {
-                    let style = window.getComputedStyle(elementList[i]);
-                    let lastValue = style.getPropertyValue(prop); 
-                    elementList[i].style.setProperty(lastValue, value);
+                    elementList[i].classList.remove(remoteClass);
                 }
-            } else {
-                let style = window.getComputedStyle(elementList[0]);
-                
-                return style.getPropertyValue(prop);
-            }
 
-            return this;
-        },
-        click: function(handler) {
-            for (let i = 0; i < elementList.length; ++i) {   
-                elementList[i].addEventListener('click', handler, false);
-            }
-            
-            return this;
-        },
-        each: function(callback) {
-            for (let i = 0; i < elementList.length; ++i) {   
-                callback.call(elementList[i]);
-            }
-            
-            return this;
-        },
-        toggle: function() {
-            return this;
-        },
-        wrap: function() {
-            return this;
-        }
-    }
-}
+                return this;
+            },
+            append(data) {
+                for (let i = 0; i < elementList.length; ++i) {
+                    const lastValue = elementList[i].innerHTML;
+                    elementList[i].innerHTML = `${lastValue} ${data}`;
+                }
 
-// myjQuery(".lala").addClass("kis").removeClass("lala");
-// myjQuery("body").remove("#para1");
-// myjQuery(".lala").append("hello");
-// myjQuery("span").empty();
-// myjQuery("#dd").children();
-// myjQuery("#dd").attr("width", "400px");
-// myjQuery("span").each(function () {
-//     console.log(this);
-// });
-// myjQuery(".lala").click(function() {
-//     alert('hello');
-// });
-// myjQuery(".lala").css('font-size');
-// myjQuery(".lala").css('color', "red");
+                return this;
+            },
+            remove(elementName) {
+                if (elementName == null) {
+                    for (let i = 0; i < elementList.length; ++i) {
+                        elementList[i].remove();
+                    }
+                } else {
+                    const elem = document.createElement(elementName);
+
+                    for (let i = 0; i < elementList.length; ++i) {
+                        const children = elementList[i].childNodes;
+
+                        for (const child of children) {
+                            elem.innerHTML = child.innerHTML;
+
+                            if (child.nodeType != 3 && child.isEqualNode(elem)) {
+                                child.remove();
+                            }
+                        }
+                    }
+                }
+
+                return this;
+            },
+            text(content) {
+                if (content == null) {
+                    let commonValue = "";
+
+                    for (let i = 0; i < elementList.length; ++i) {
+                        commonValue = `${commonValue + elementList[i].innerHTML} `;
+                    }
+
+                    return commonValue;
+                }
+
+                for (let i = 0; i < elementList.length; ++i) {
+                    elementList[i].innerHTML = content;
+                }
+
+                return this;
+            },
+            attr(attribute, valueAttribute) {
+                if (value == null) {
+                    return elementList[0].getAttribute(attribute);
+                }
+                elementList[0].setAttribute(attribute, valueAttribute);
+
+                return this;
+            },
+            children() {
+                const childNode = [];
+
+                for (const elem of elementList) {
+                    const children = elem.childNodes;
+
+                    for (let i = 0; i < children.length; ++i) {
+                        if (children[i].nodeType != 3) {
+                            childNode.push(children[i]);
+                        }
+                    }
+                }
+            },
+            empty() {
+                for (let i = 0; i < elementList.length; ++i) {
+                    elementList[i].innerHTML = "";
+                }
+
+                return this;
+            },
+            css(prop, valueProperty) {
+                if (valueProperty != null) {
+                    for (let i = 0; i < elementList.length; ++i) {
+                        const style = window.getComputedStyle(elementList[i]);
+                        const lastValue = style.getPropertyValue(prop);
+                        elementList[i].style.setProperty(lastValue, valueProperty);
+                    }
+                } else {
+                    const style = window.getComputedStyle(elementList[0]);
+
+                    return style.getPropertyValue(prop);
+                }
+
+                return this;
+            },
+            click(handler) {
+                for (let i = 0; i < elementList.length; ++i) {
+                    elementList[i].addEventListener("click", handler, false);
+                }
+
+                return this;
+            },
+            each(callback) {
+                for (let i = 0; i < elementList.length; ++i) {
+                    callback.call(elementList[i]);
+                }
+
+                return this;
+            },
+            toggle() {
+                for (let i = 0; i < elementList.length; ++i) {
+                    if (elementList[i].hidden) {
+                        elementList[i].hidden = false;
+                    } else {
+                        elementList[i].hidden = true;
+                    }
+                }
+
+                return this;
+            },
+            wrap(elem) {
+                const parent = elementList[0].parentNode;
+                elementList[0].insertAdjacentHTML("afterEnd", elem);
+
+                for (let i = 0; i < parent.childNodes.length; ++i) {
+                    if (parent.childNodes[i].isEqualNode(elementList[0])) {
+                        console.log(parent.childNodes[i + 1]);
+                        parent.childNodes[i + 1].appendChild(elementList[0]);
+                    }
+                }
+
+                return this;
+            },
+        };
+    };
+}, false);
